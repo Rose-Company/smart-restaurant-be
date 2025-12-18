42fda2dcaf142fee5549b70b01ea73c11b22d5d6
mainhatnam219 <22120219@student.hcmus.edu.vn>
2025-12-18 23:17:48 +0700

    remove old logic

--- common/error_messages.go
+++ common/error_messages.go
@@ -3,11 +3,8 @@ package common
 import (
 	"errors"
 	"fmt"
-	"net/http"
 	"strings"
 	"sync"
-
-	"gorm.io/gorm"
 
 	"github.com/gin-gonic/gin"
 )
@@ -58,101 +55,11 @@ var PgErrorTransform = func(err error) error {
 }
 
 var (
-	ErrCodeNotAuthorized                            = errors.New("not_authorized")
-	ErrCartCodeNotFound                             = errors.New("cart_not_found")
-	ErrCodeSystemErr                                = errors.New("system_error")
-	ErrCodeInvalidData                              = errors.New("invalid_data")
-	ErrCodeItemNotInCart                            = errors.New("item_not_in_cart")
-	ErrCodeItemNotFound                             = errors.New("item_not_found")
-	ErrCodeInvalidCartStatus                        = errors.New("invalid_cart_status")
-	ErrCodeInvalidItemQTY                           = errors.New("invalid_item_qty")
-	ErrCodeInvalidTotalPrice                        = errors.New("invalid_total_price")
-	ErrCodeYouAlreadyInClass                        = errors.New("you_already_in_class")
-	ErrCodeHasInvalidItemInCart                     = errors.New("has_invalid_item_in_cart")
-	ErrCodeClassClosed                              = errors.New("class_closed")
-	ErrCodeClassNotFound                            = errors.New("class_not_found")
-	ErrCodeUserDeactivated                          = errors.New("user_deactivated")
-	ErrCodeTokenExpired                             = errors.New("token_expired")
-	ErrCodeItemAlreadyExistsInOtherOrder            = errors.New("item_already_exists_in_other_order")
-	ErrNotAuthorized                                = errors.New("not_authorized")
-	ErrNotAllowTrialMode                            = errors.New("not_allow_trial_mode")
-	ErrCodeGenerateExamFailed                       = errors.New("generate_exam_failed")
-	ErrCodeYouAreNotInClass                         = errors.New("you_are_not_in_class")
-	ErrCodeQuizNotFound                             = errors.New("quiz_not_found")
-	ErrCodeRegistrationRequestNotFound              = errors.New("registration_request_not_found")
-	ErrCodeUserAlreadyExists                        = errors.New("user_already_exists")
-	ErrCodeUserNotExists                            = errors.New("user_not_exists")
-	ErrCodeEmailAlreadyExists                       = errors.New("email_already_exists")
-	ErrNotFoundRegistrationRequest                  = errors.New("not_found_registration_request")
-	ErrInvalidRegistrationRequestPaymentStatus      = errors.New("invalid_registration_request_payment_status")
-	ErrInvalidRegistrationRequestEntranceTestStatus = errors.New("invalid_registration_request_entrance_test_status")
-	ErrInvalidRegistrationEnrollStatus              = errors.New("invalid_registration_request_enroll_status")
-	ErrMissingEmail                                 = errors.New("missing_email")
-	ErrActionNotAllowed                             = errors.New("action_not_allowed")
-	ErrTokenNotFound                                = errors.New("token_not_found")
-	ErrInvalidIeltsBand                             = errors.New("invalid_ielts_band")
-	ErrInvalidStudyDuration                         = errors.New("invalid_study_duration")
-	ErrCodeInvalidPromoCode                         = errors.New("invalid_promo_code")
-	ErrCodePromoCodeIsExpired                       = errors.New("promo_code_is_expired")
-	ErrCodePromoCodeIsUsedForSpecificItem           = errors.New("promo_code_is_used_for_specific_item")
-	ErrCodePromoCodeIsUsedForMinAmount              = errors.New("promo_code_is_used_for_min_amount")
-	ErrCodePromoCodeIsExceedMaxUse                  = errors.New("promo_code_is_exceed_max_use")
-	ErrCodePromoCodeIsUsedForSpecificUser           = errors.New("promo_code_is_used_for_specific_user")
-	ErrCodePromoCodeIsUsedForNewUser                = errors.New("promo_code_is_used_for_new_user")
-	ErrCodeOrderNotFound                            = errors.New("order_not_found")
-	ErrCodeInvalidOrderStatus                       = errors.New("invalid_order_status")
-	ErrNotFoundEnv                                  = errors.New("not found env")
-	ErrAlertFailed                                  = errors.New("alert failed")
-	ErrNoQuestionsInQuiz                            = errors.New("no_questions_in_quiz")
-	ErrRecordNotFound                               = errors.New("record_not_found")
-	ErrQuizNotInClass                               = errors.New("quiz_not_in_class")
-	ErrUserNotInClass                               = errors.New("user_not_in_class")
-	ErrAnswerStatisticTypeRequired                  = errors.New("answer_statistic_type_required")
-	ErrAnswerStatisticTypeInvalid                   = errors.New("answer_statistic_type_invalid")
-	ErrNextExamDateBeforeNow                        = errors.New("next_exam_date_before_now")
-	ErrInvalidVotes                                 = errors.New("invalid_votes")
-	ErrNotFound                                     = gorm.ErrRecordNotFound
-	ErrNoDataToUpdate                               = errors.New("no_data_to_update")
-	ErrNotUpdateBothBandScoreAndBandScore100        = errors.New("not_update_both_band_score_and_band_score_100")
-	ErrInvalidBandScore                             = errors.New("invalid_band_score")
-	ErrInvalidAnswerStatus                          = errors.New("invalid_answer_status")
-	ErrAnswerStatusReviewed                         = errors.New("answer_status_is_reviewed")
-	ErrAnswerClassNull                              = errors.New("answer_class_is_null")
-	ErrRecordExisted                                = errors.New("record_existed")
-	ErrNoStudentsInClass                            = errors.New("no_students_in_class")
-	ErrNoQuizzesInClass                             = errors.New("no_quizzes_in_class")
-	ErrAnswerNotReviewedYet                         = errors.New("answer_not_reviewed_yet")
-	ErrReachMaxRetries                              = errors.New("reach_max_retries")
-	ErrReviewNotFound                               = errors.New("review_not_found")
-	ErrInReviewing                                  = errors.New("in reviewing")
-	ErrNotFoundRegistrations                        = errors.New("not_found_registration")
-	ErrNotFoundStudent                              = errors.New("not_found_student")
-	ErrNotFoundCourse                               = errors.New("not_found_course")
-	ErrNoValidStudents                              = errors.New("no valid students")
-	ErrNoValidClasses                               = errors.New("no valid classes")
-	ErrNoValidCourses                               = errors.New("no valid courses")
-	ErrNoValidRegistrationRequests                  = errors.New("no valid registration requests")
-	ErrMaxAttachementExceeded                       = errors.New("max_attachment_exceeded")
-	ErrInvalidObjectType                            = errors.New("invalid_object_type")
-	ErrUpdateFailed                                 = errors.New("update_failed")
-	ErrInvalidEmail                                 = errors.New("no_email")
-	ErrInvalidRequest                               = errors.New("invalid_request")
-	ErrInvalidAnswerID                              = errors.New("invalid Answer ID")
-	ErrNoSlotAIReviewWriting                        = errors.New("student has run out of turns grading with AI")
-	ErrInvalidWritingAnswer                         = errors.New("invalid writing answer")
-	ErrNotFoundPosition                             = errors.New("not found position")
-	ErrNoSlotAIVocabSuggest                         = errors.New("student has run out of turn using vocab suggestion")
-	ErrInvalidCategory                              = errors.New("invalid category")
-	ErrInvalidVocabValue                            = errors.New("invalid vocab value")
-	ErrCategoryNotFound                             = errors.New("category not found in default categories")
-	ErrVocabNotFound                                = errors.New("vocab entry not found or does not belong to the user")
-	ErrCategoryNull                                 = errors.New("category must not be empty")
-	ErrValueNull                                    = errors.New("value must not be empty")
-	ErrNotRetrieveCategory                          = errors.New("cannot retrieve category")
-	ErrInvalidVocabStatus                           = errors.New("invalid vocab status")
-	ErrInvalidPromoCodeApplyContext                 = errors.New("invalid_promo_code_apply_context")
-	ErrWrongLoginInfo                               = errors.New("wrong_login_info")
-	ErrRegistrationRequestIsCancelled               = errors.New("registration request is cancelled")
+	ErrCodeNotAuthorized = errors.New("not_authorized")
+	ErrCodeSystemErr     = errors.New("system_error")
+	ErrActionNotAllowed  = errors.New("action_not_allowed")
+	ErrTokenNotFound     = errors.New("token_not_found")
+	ErrNotAuthorized     = errors.New("not_authorized")
 )
 
 var (
@@ -425,184 +332,10 @@ var listErrorData = []errData{
 		MessageEnUs: "Token not found",
 	},
 	{
-		Code:        "invalid_ielts_band",
-		HTTPCode:    400,
-		MessageViVn: "Điểm số ielts không hợp lệ",
-		MessageEnUs: "Invalid band",
-	},
-	{
-		Code:        "invalid_study_duration",
-		HTTPCode:    400,
-		MessageViVn: "Số giờ học không hợp lệ",
-		MessageEnUs: "Invalid study duration",
-	},
-	{
-		Code:        ErrRecordNotFound.Error(),
-		HTTPCode:    http.StatusNotFound,
-		MessageViVn: "Record not found",
-		MessageEnUs: "Record not found",
-	},
-	{
-		Code:        ErrQuizNotInClass.Error(),
-		HTTPCode:    http.StatusNotFound,
-		MessageViVn: "Quiz không thuộc lớp học",
-		MessageEnUs: "Quiz is not in the class",
-	},
-	{
-		Code:        ErrUserNotInClass.Error(),
-		HTTPCode:    http.StatusNotFound,
-		MessageViVn: "Người dùnng không thuộc lớp học",
-		MessageEnUs: "User is not in the class",
-	},
-	{
-		Code:        ErrAnswerStatisticTypeRequired.Error(),
-		HTTPCode:    http.StatusBadRequest,
-		MessageViVn: "Type là bắt buộc, vui lòng nhập thông tin cho field này",
-		MessageEnUs: "Required for type",
-	},
-	{
-		Code:        ErrAnswerStatisticTypeInvalid.Error(),
-		HTTPCode:    http.StatusBadRequest,
-		MessageViVn: "Type không hợp lệ, vui lòng nhập thông tin hợp lệ",
-		MessageEnUs: "Type is invalid",
-	},
-	{
-		Code:        ErrNextExamDateBeforeNow.Error(),
-		HTTPCode:    http.StatusBadRequest,
-		MessageViVn: "Thời gian thi phải sau thời gian hiện tại",
-		MessageEnUs: "Exam date must be after now",
-	},
-	{
-		Code:        ErrNotFound.Error(),
-		HTTPCode:    http.StatusNotFound,
-		MessageViVn: "Không tìm thấy",
-		MessageEnUs: "Not found",
-	},
-	{
-		Code:        ErrNoDataToUpdate.Error(),
-		HTTPCode:    http.StatusBadRequest,
-		MessageViVn: "Không có thông tin để cập nhật",
-		MessageEnUs: "No data to update",
-	},
-	{
-		Code:        ErrNotUpdateBothBandScoreAndBandScore100.Error(),
-		HTTPCode:    http.StatusBadRequest,
-		MessageViVn: "Không thể cập nhật đồng thời điểm theo ielts và điểm theo thang 100",
-		MessageEnUs: "Can't update both band score and band score with scale 100",
-	},
-	{
-		Code:        ErrInvalidBandScore.Error(),
-		HTTPCode:    http.StatusBadRequest,
-		MessageViVn: "Điểm số không hợp lệ",
-		MessageEnUs: "Band score/band score with scale 100 is invalid",
-	},
-	{
-		Code:        ErrInvalidAnswerStatus.Error(),
-		HTTPCode:    http.StatusBadRequest,
-		MessageViVn: "Trạng thái của câu trả lời không hợp lệ",
-		MessageEnUs: "Answer status is invalid",
-	},
-	{
-		Code:        ErrAnswerStatusReviewed.Error(),
-		HTTPCode:    http.StatusBadRequest,
-		MessageViVn: "Câu trả lời đã được chấm điểm, vui lòng không cập nhật thông tin",
-		MessageEnUs: "Answer status is invalid",
-	},
-	{
-		Code:        ErrAnswerClassNull.Error(),
-		HTTPCode:    http.StatusBadRequest,
-		MessageViVn: "Lớp học ở câu trả lời không có thông tin",
-		MessageEnUs: "Answer class is null",
-	},
-	{
 		Code:        ErrActionNotAllowed.Error(),
-		HTTPCode:    http.StatusForbidden,
-		MessageViVn: "Không có quyền thực hiện thao tác",
-		MessageEnUs: "Action is not allowed",
-	},
-	{
-		Code:        ErrRecordExisted.Error(),
-		HTTPCode:    http.StatusConflict,
-		MessageViVn: "Record đã tồn tại",
-		MessageEnUs: "Record is already existed",
-	},
-	{
-		Code:        ErrNoStudentsInClass.Error(),
-		HTTPCode:    http.StatusBadRequest,
-		MessageViVn: "Lớp không có học sinh",
-		MessageEnUs: "Class doesn't have any student",
-	},
-	{
-		Code:        ErrNoQuizzesInClass.Error(),
-		HTTPCode:    http.StatusBadRequest,
-		MessageViVn: "Lớp không có các quiz hợp lệ",
-		MessageEnUs: "Class doesn't have any valid quizzes",
-	},
-	{
-		Code:        ErrAnswerNotReviewedYet.Error(),
-		HTTPCode:    http.StatusBadRequest,
-		MessageViVn: "Bài làm chưa được review",
-		MessageEnUs: "Answer hasn't been reviewed",
-	},
-	{
-		Code:        ErrReviewNotFound.Error(),
-		HTTPCode:    http.StatusNotFound,
-		MessageViVn: "Review không tồn tại",
-		MessageEnUs: "Review is not found",
-	},
-	{
-		Code:        ErrMaxAttachementExceeded.Error(),
-		HTTPCode:    http.StatusBadRequest,
-		MessageViVn: "Số lượng file đính kèm vượt quá giới hạn",
-		MessageEnUs: "Number of attachments exceeds the limit",
-	},
-	{
-		Code:        ErrInvalidObjectType.Error(),
-		HTTPCode:    http.StatusBadRequest,
-		MessageViVn: "Loại đối tượng không hợp lệ",
-		MessageEnUs: "Invalid object type",
-	},
-	{
-		Code:        ErrUpdateFailed.Error(),
-		HTTPCode:    http.StatusBadRequest,
-		MessageViVn: "Cập nhật thất bại",
-		MessageEnUs: "Update failed",
-	},
-	{
-		Code:        ErrInvalidEmail.Error(),
-		HTTPCode:    http.StatusBadRequest,
-		MessageViVn: "Email không hợp lệ",
-		MessageEnUs: "Invalid email",
-	},
-	{
-		Code:        ErrInvalidRequest.Error(),
-		HTTPCode:    http.StatusBadRequest,
-		MessageViVn: "Yêu cầu không hợp lệ",
-		MessageEnUs: "Invalid request",
-	},
-	{
-		Code:        ErrNotFoundPosition.Error(),
-		HTTPCode:    http.StatusBadRequest,
-		MessageViVn: "Vị trí không hợp lệ",
-		MessageEnUs: "Invalid position",
-	},
-	{
-		Code:        ErrInvalidPromoCodeApplyContext.Error(),
-		HTTPCode:    http.StatusBadRequest,
-		MessageViVn: "Mã khuyến mại không áp dụng cho đối tượng này",
-		MessageEnUs: "Promo code is not applied for this context",
-	},
-	{
-		Code:        ErrWrongLoginInfo.Error(),
-		HTTPCode:    http.StatusUnauthorized,
-		MessageViVn: "Thông tin đăng nhập không đúng",
-		MessageEnUs: "Login information is wrong",
-	},
-	{
-		Code:        ErrRegistrationRequestIsCancelled.Error(),
-		HTTPCode:    http.StatusBadRequest,
-		MessageViVn: "Yêu cầu đăng ký đã bị hủy",
-		MessageEnUs: "Registration request is cancelled",
+		HTTPCode:    403,
+		MessageViVn: "Hành động không được phép",
+		MessageEnUs: "Action not allowed",
 	},
 }
 
